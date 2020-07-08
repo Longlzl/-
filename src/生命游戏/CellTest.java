64d187031c2508c00a27f9583999d5aac12dea93
longbuger <1792144731@qq.com>
2020-07-08 19:13:02 +0800

    ddd

---
+++ src/生命游戏/CellTest.java
@@ -0,0 +1,24 @@
+package 生命游戏;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CellTest {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Test
+  public void test() {
+    fail("尚未实现");
+  }
+
+}
